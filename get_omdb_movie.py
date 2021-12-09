05a1f4f67cecd0e191923aeb02bad9eb656e1195
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-12-09 22:12:23 +0100

    add key

--- get_omdb_movie.py
+++ get_omdb_movie.py
@@ -3,9 +3,10 @@ import sys
 from omdb_client import OmdbApiClient
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     if len(sys.argv) == 1:
         raise ValueError("Unsupported amount of arguments provided")
-    omdb_key = '875f3794'    
-    omdb_client = OmdbApiClient()
-    print(omdb_client.get_omdb_movie_info(sys.argv[1])
+    omdb_key = '875f3794'
+    omdb_client = OmdbApiClient(omdb_key)
+    print(omdb_client.get_rotten_tomato_rating(sys.argv[1]))
+
