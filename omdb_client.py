fc8fbee632eb0304ae1c4f1097db2dd4fd9f6d5c
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-12-09 21:57:14 +0100

    Add get_rotten_tomato_rating logic

--- omdb_client.py
+++ omdb_client.py
@@ -1,15 +1,30 @@
+import json
 import requests
 
 
 class OmdbApiClient:
-  def __init__(self, api_key):
-    self.key = api_key
-    self.url = 'http://www.omdbapi.com/'
-        
-  def get_movie_info(self, title):
-    get_url = f'{self.url}?apikey={self.key}&t={title}' 
-    raw_output = requests.get(get_url)
-    return raw_output.text
-    
-  def get rotten_tomato_rating(title):
-    pass
+    def __init__(self, api_key):
+        self.key = api_key
+        self.url = 'http://www.omdbapi.com/'
+
+    def get_movie_info(self, title):
+        get_url = f'{self.url}?apikey={self.key}&t={title}'
+        raw_output = requests.get(get_url)
+        pretty_output = json.loads(raw_output.text)
+
+        assert pretty_output['Response'] == 'True', \
+            f'Movie: {title} was not found!'
+        return pretty_output
+
+    def get_rotten_tomato_rating(self, title):
+        movie_info = self.get_movie_info(title)
+
+        assert movie_info.get('Ratings'), f"Movie: {title} has no Ratings"
+        ratings = movie_info['Ratings']
+
+        assert 'Rotten Tomatoes' in [source['Source'] for source in ratings],\
+            f"Movie: {title} has no Rotten Tomatoes ratings"
+
+        for rating in ratings:
+            if rating['Source'] == 'Rotten Tomatoes':
+                return rating['Value']
