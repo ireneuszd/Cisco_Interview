8fa256fbffa25e2df57bda51448b138792c93306
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-12-09 23:28:32 +0100

    Remove key from get_omdb_movie.py

--- get_omdb_movie.py
+++ get_omdb_movie.py
@@ -1,3 +1,4 @@
+from decouple import config
 import sys
 
 from omdb_client import OmdbApiClient
@@ -6,7 +7,7 @@ from omdb_client import OmdbApiClient
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         raise ValueError("Unsupported amount of arguments provided")
-    omdb_key = '875f3794'
-    omdb_client = OmdbApiClient(omdb_key)
-    print(omdb_client.get_rotten_tomato_rating(sys.argv[1]))
+    omdb_client = OmdbApiClient(config('OMDB_KEY'))
 
+    rating = omdb_client.get_rotten_tomato_rating(sys.argv[1])
+    print(f'Rotten Tomato rating for movie: {sys.argv[1]} is:\n {rating}')
